efd3e6e45bf9bbd25bcce041e140f2fb296a21f6
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-11-23 15:25:28 +0100

    add node connection

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
 /* GET Hello World. */
 router.get('/hello-world', (req, res, next) => {
   res.render('helloworld', { title: 'Hello World' });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', (req, res) => {
+  const { db } = req;
+  const collection = db.get('usercollection');
+  collection.find({}, {}, (e, docs) => {
+      res.render('userlist', {
+          "userlist" : docs
+      });
+  });
 });
 
 module.exports = router;
