661d9229ecacf0103e8997b676267989544b4b94
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-11-23 15:57:14 +0100

    add new user endpoint

--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,38 @@ router.get('/userlist', (req, res) => {
   });
 });
 
+/* GET New User page. */
+router.get('/newuser', (req, res) => {
+  res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', (req, res) => {
+
+  // Set our internal DB variable
+  const { db } = req;
+
+  // Get our form values. These rely on the "name" attributes
+  const userName = req.body.username;
+  const userEmail = req.body.useremail;
+
+  // Set our collection
+  const collection = db.get('usercollection');
+
+  // Submit to the DB
+  collection.insert({
+      "username": userName,
+      "email": userEmail
+  }, (err, doc) => {
+      if (err) {
+          // If it failed, return error
+          res.send("There was a problem adding the information to the database.");
+      }
+      else {
+          // And forward to success page
+          res.redirect("userlist");
+      }
+  });
+});
+
 module.exports = router;
